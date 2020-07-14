65d0c24456c9fefd1e300132577233f048beec9e
leo <coderleo42+leocodes@gmail.com>
2020-07-13 21:41:41 -0400

    skip compressed "chunks" completely

--- Chunky/IO/ChunkBundleLoader.cs
+++ Chunky/IO/ChunkBundleLoader.cs
@@ -24,7 +24,7 @@ namespace Chunky.IO
                 while (stream.Position < stream.Length)
                 {
                     var chunk = NextChunk(reader);
-                    if (chunk.Id == 0) continue;
+                    if (chunk == null || chunk.Id == 0) continue;
                     var resource = LoadResource(reader, chunk);
 
                     resources.Add(resource);
@@ -72,10 +72,33 @@ namespace Chunky.IO
         private Chunk NextChunk(BinaryReader reader)
         {
             var id = reader.ReadUInt32();
+
+            // Ugly but necessary hardcoded check.
+            // If we encounter data with the CompressInPlace marker, we should parse the CIPHeader structure and just skip over the data.
+            // The resource handler will have to deal with compressed data on its own. This is usually pretty simple, because offsets to compressed blocks
+            // are usually found in an *actual* chunk.
+            if (id == 0x55441122)
+            {
+                // read USize
+                reader.ReadUInt32();
+
+                // read CSize
+                var compressedSize = reader.ReadUInt32();
+
+                if (compressedSize < 12) throw new ChunkReaderException("Invalid size in compressed data block");
+
+                if (reader.BaseStream.Position + (compressedSize - 12) > reader.BaseStream.Length)
+                    throw new ChunkReaderException("Overflowing compressed data block");
+
+                // skip to next block
+                reader.BaseStream.Position += compressedSize - 12;
+
+                return null;
+            }
+
             var size = reader.ReadInt32();
 
-            if (id != 0x55441122 /* Don't check chunk size for compressed data */ &&
-                reader.BaseStream.Position + size > reader.BaseStream.Length)
+            if (reader.BaseStream.Position + size > reader.BaseStream.Length)
                 throw new ChunkReaderException($"Overflowing chunk detected at {reader.BaseStream.Position - 8}.");
 
             var chunk = new Chunk {Id = id, Size = size, Offset = reader.BaseStream.Position - 8};
@@ -85,7 +108,7 @@ namespace Chunky.IO
                 while (reader.BaseStream.Position < chunk.EndOffset)
                 {
                     var nextChunk = NextChunk(reader);
-                    if (nextChunk.Id == 0) continue;
+                    if (nextChunk == null || nextChunk.Id == 0) continue;
                     chunk.Children.Add(nextChunk);
                 }
 
