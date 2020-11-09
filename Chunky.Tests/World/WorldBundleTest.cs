fc2f3e2e080eb74b255d08a5f261452a3f607af2
leo <coderleo42+leocodes@gmail.com>
2020-11-09 09:15:56 -0500

    add test for World car bundle with compressed chunks

---
+++ Chunky.Tests/World/WorldBundleTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chunky.Tests.World
+{
+    [TestClass]
+    public class WorldBundleTest
+    {
+        [TestMethod]
+        public void TestBundleWithCompressInPlace()
+        {
+            using Stream stream = File.OpenRead(@"test-data\world\GEOMETRY.BIN");
+            var bundle = Bundle.FromStream(stream);
+            Assert.AreEqual(bundle.Count(), 1);
+        }
+    }
+}
