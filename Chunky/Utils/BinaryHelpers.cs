356a3798ca8f250fcaad0956db405edeb3a1f2f8
leo <coderleo42+leocodes@gmail.com>
2020-11-09 11:46:09 -0500

    Add binary helper functions in the form of BinaryHelpers class

---
+++ Chunky/Utils/BinaryHelpers.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Chunky.Utils
+{
+    /// <summary>
+    ///     Exposes helper functions for working with binary streams.
+    /// </summary>
+    public static class BinaryHelpers
+    {
+        /// <summary>
+        ///     Reads a structure from a binary stream.
+        /// </summary>
+        /// <param name="reader">The stream to read from.</param>
+        /// <typeparam name="T">The structure type.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" /> read from the stream.</returns>
+        public static T ReadStruct<T>(BinaryReader reader) where T : struct
+        {
+            return ReadStruct<T>(reader.BaseStream);
+        }
+
+        /// <summary>
+        ///     Reads a structure from a binary stream.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <typeparam name="T">The structure type.</typeparam>
+        /// <returns>An instance of <typeparamref name="T" /> read from the stream.</returns>
+        public static T ReadStruct<T>(Stream stream) where T : struct
+        {
+            var size = Marshal.SizeOf<T>();
+            var buffer = ReadBytesRequired(stream, size);
+            using var handle = new DisposableGcHandle(buffer, GCHandleType.Pinned);
+
+            return Marshal.PtrToStructure<T>(handle.AddrOfPinnedObject());
+        }
+
+        /// <summary>
+        ///     Reads an array of bytes from a binary stream,
+        ///     ensuring the data is completely read.
+        /// </summary>
+        /// <param name="reader">The stream to read from.</param>
+        /// <param name="length">The number of bytes to read.</param>
+        /// <returns>An array of bytes read from the stream.</returns>
+        /// <exception cref="InvalidDataException">if the data cannot be read completely</exception>
+        public static byte[] ReadBytesRequired(BinaryReader reader, int length)
+        {
+            return ReadBytesRequired(reader.BaseStream, length);
+        }
+
+        /// <summary>
+        ///     Reads an array of bytes from a binary stream,
+        ///     ensuring the data is completely read.
+        /// </summary>
+        /// <param name="stream">The stream to read from.</param>
+        /// <param name="length">The number of bytes to read.</param>
+        /// <returns>An array of bytes read from the stream.</returns>
+        /// <exception cref="InvalidDataException">if the data cannot be read completely</exception>
+        public static byte[] ReadBytesRequired(Stream stream, int length)
+        {
+            var buffer = new byte[length];
+            if (stream.Read(buffer, 0, buffer.Length) != buffer.Length)
+                throw new InvalidDataException($"Could not read {buffer.Length} bytes from stream");
+
+            return buffer;
+        }
+
+        /// <summary>
+        ///     Marshals a structure to a byte array.
+        /// </summary>
+        /// <param name="data">The structure to marshal.</param>
+        /// <typeparam name="T">The structure type.</typeparam>
+        /// <returns>An array of bytes representing the structure.</returns>
+        public static byte[] MarshalStruct<T>(T data) where T : struct
+        {
+            var size = Marshal.SizeOf<T>();
+            var buffer = new byte[size];
+
+            using var gcHandle = new DisposableGcHandle(buffer, GCHandleType.Pinned);
+            Marshal.StructureToPtr(data, gcHandle.AddrOfPinnedObject(), false);
+
+            return buffer;
+        }
+
+        /// <summary>
+        ///     Writes a structure to a binary stream.
+        /// </summary>
+        /// <param name="writer">The binary stream to write the structure to.</param>
+        /// <param name="data">The structure to write.</param>
+        /// <typeparam name="T">The structure type.</typeparam>
+        public static void WriteStruct<T>(BinaryWriter writer, T data) where T : struct
+        {
+            WriteStruct(writer.BaseStream, data);
+        }
+
+
+        /// <summary>
+        ///     Writes a structure to a binary stream.
+        /// </summary>
+        /// <param name="stream">The binary stream to write the structure to.</param>
+        /// <param name="data">The structure to write.</param>
+        /// <typeparam name="T">The structure type.</typeparam>
+        public static void WriteStruct<T>(Stream stream, T data) where T : struct
+        {
+            stream.Write(MarshalStruct(data));
+        }
+    }
+}
---
+++ Chunky/Utils/DisposableGCHandle.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+namespace Chunky.Utils
+{
+    [SecurityPermissionAttribute(SecurityAction.InheritanceDemand, Flags = SecurityPermissionFlag.UnmanagedCode)]
+    public class DisposableGcHandle : IDisposable
+    {
+        private GCHandle _handle;
+
+        public DisposableGcHandle(object value)
+        {
+            _handle = GCHandle.Alloc(value, GCHandleType.Normal);
+        }
+
+        public DisposableGcHandle(object value, GCHandleType type)
+        {
+            _handle = GCHandle.Alloc(value, type);
+        }
+
+        public IntPtr AddrOfPinnedObject()
+        {
+            return _handle.AddrOfPinnedObject();
+        }
+
+        public void Free()
+        {
+            _handle.Free();
+        }
+
+        #region Properties
+
+        public object Target
+        {
+            get => _handle.Target;
+            set => _handle.Target = value;
+        }
+
+        public bool IsAllocated => _handle.IsAllocated;
+
+        #endregion
+
+        #region DisposePattern
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                if (_handle.IsAllocated)
+                    _handle.Free();
+
+            GC.SuppressFinalize(this);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public void Close()
+        {
+            Dispose(true);
+        }
+
+        ~DisposableGcHandle()
+        {
+            Dispose(false);
+        }
+
+        #endregion
+    }
+}
